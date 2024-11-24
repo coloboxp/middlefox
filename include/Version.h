62abaccf110cde6290247747c7dbb6ef493c6e22
Alejandro <coloboxp@gmail.com>
2024-11-24 12:27:38 +0100

    Added versioning outside module

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.3"
+        #define VERSION "0.1.4"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "24.Nov.24 115115"
+        #define BUILD_TIMESTAMP "24.Nov.24 122543"
     #endif
     
